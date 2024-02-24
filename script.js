7b06b40d01550987a7e8d640047501614f9ec7c3
DeLeonLabUF <161155984+DeLeonLabUF@users.noreply.github.com>
2024-02-24 17:07:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,5 @@ var version = '1.12.1';
 
 initializeSketchfab('271bd5c6d9854fe6997f8a31475e9d2c', 'api-frame-dog', 'Calotte_v6_2');
 initializeSketchfab('423318aa8fd14782976d528fd8e2a5d0', 'api-frame-other', null);
+
+// I am grateful to the Sketchfab Developer Team and the video tutorial by Klaas Nienhuis (https://www.klaasnienhuis.nl) on YouTube (https://www.youtube.com/live/mVQNDCwbXMM)!
