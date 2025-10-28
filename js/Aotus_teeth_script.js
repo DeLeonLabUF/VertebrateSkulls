c67ea461be051ae11e43bdb53278ff5b5642e241
DeLeonLabUF <161155984+DeLeonLabUF@users.noreply.github.com>
2025-10-28 13:43:54 -0400

    Update Aotus_teeth_script.js

--- js/Aotus_teeth_script.js
+++ js/Aotus_teeth_script.js
@@ -31,6 +31,21 @@ function initializeSketchfab(uid, iframeId, modelName = "") {
   });
 }
 
+/* ======= LIST ALL NODES ======== */
+
+function logAllNodes(api, modelName = "") {
+  api.getNodeMap(function (err, nodes) {
+    if (err) {
+      console.error("Error getting node map:", err);
+      return;
+    }
+    console.group(`Nodes in model: ${modelName}`);
+    Object.values(nodes).forEach((node) =>
+      console.log(`${node.name} (instanceID: ${node.instanceID}, parentID: ${node.parentID})`)
+    );
+    console.groupEnd();
+  });
+}
 /* ====== SUCCESS HANDLER ====== */
 
 function success(api, iframeId, modelName) {
@@ -41,7 +56,9 @@ function success(api, iframeId, modelName) {
         console.error("Error getting node map:", err);
         return;
       }
-
+      
+      logAllNodes(api, modelName);
+      
       // Convert nodes object to an array for easier filtering
       const nodeArray = Object.values(nodes);
 
@@ -102,5 +119,7 @@ function addClickEvent(api, nodeIDs) {
 initializeSketchfab("6920446710f64de3a6621ac766255d16", "DWA-Aotus1_Teeth", "Aotus1");
 initializeSketchfab("9fb6cf5aac834deeac18c92e50cf9259", "Aotus107_Teeth", "Aotus107");
 initializeSketchfab("f5696596b5bc4338ba1b21a49ee1955e", "Aotus108_Teeth", "Aotus108");
+
+
 
 // Author: Valerie Burke DeLeon using codepen.io; I am grateful to the Sketchfab Developer Team and the video tutorial by Klaas Nienhuis (https://www.klaasnienhuis.nl) on YouTube (https://www.youtube.com/live/mVQNDCwbXMM)!
